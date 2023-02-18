fe51b62596a525fd85c7e799ae173765490e08ff
satyam raj chaudhary <rajsatyam200@gmail.com>
2023-02-19 01:15:34 +0530

    poemt

---
+++ basic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner ;
+class basic {
+  public static void main(String[] args) {
+    int a,b,sum ;
+    Scanner obj=new Scanner(System.in);
+    System.out.printf("enter your two number=");
+    a=obj.nextInt();
+    b=obj.nextInt();
+    sum=a+b;
+    System.out.print("you output is:" + sum);
+        
+    }
+}
